8f411c4a19fedc2e857f94bebf902433a6557d03
François Poirotte <clicky@erebot.net>
2011-01-04 17:10:36 +0000

    Missing license header.

--- bootstrap.php
+++ bootstrap.php
@@ -1,4 +1,20 @@
 <?php
+/*
+    This file is part of Erebot.
+
+    Erebot is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Erebot is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Erebot.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 // Avoid harmless warning on some
 // badly-configured PHP installations.
@@ -187,3 +203,4 @@ foreach (
         !interface_exists($preload, TRUE))
         throw new Exception('Could not preload "'.$preload.'"');
 unset($preload);
+
